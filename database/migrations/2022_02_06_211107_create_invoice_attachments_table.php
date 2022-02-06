d41671a25f04624bd6bd7189eb8d311e61d08c59
alikashmar8 <alikashmar8@gmail.com>
2022-02-06 23:54:27 +0200

    Invoices Attachments Added

---
+++ database/migrations/2022_02_06_211107_create_invoice_attachments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_attachments', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('note')->nullable();
+            $table->unsignedBigInteger('invoice_id');
+            $table->foreign('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_attachments');
+    }
+}
